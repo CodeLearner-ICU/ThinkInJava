699de11d18d1c23ac09b1f3cd057dc05bc791b99
62733 <fff>
2020-06-13 13:32:38 +0800

    add HelloA.java and modify Test.java for test

---
+++ src/main/java/practice/HelloA.java
@@ -0,0 +1,5 @@
+package practice;
+
+public class HelloA {
+    // TODO: 2020/6/13  doSome
+}
--- src/main/java/practice/Test.java
+++ src/main/java/practice/Test.java
@@ -1,5 +1,7 @@
 package practice;
 
 public class Test extends B implements A {
-
+    public static void main(String[] args) {
+        new Test().f();
+    }
 }
